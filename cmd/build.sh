6e45ca6360350027a464c15b86eecb00e3183667
marcos9966 <marcos.mura@gmail.com>
2019-11-03 16:49:42 +0100

    reordering package

--- cmd/build.sh
+++ cmd/build.sh
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
-go-bindata  -prefix res -pkg resmgr -nocompress -o ./internal/pkg/resmgr/assets.go res/...
-export GO111MODULE=off
-=======
 go-bindata -prefix res -pkg resmgr -nocompress -o ./internal/pkg/resmgr/assets.go res/...
->>>>>>> reordering package
 go1.13.3 build -o cmd/rhumdapp
