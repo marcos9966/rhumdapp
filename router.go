da90dd64ec34949f978a1c55f4f23315a213eb09
marcos9966 <marcos9966@gmail.com>
2019-11-07 14:59:52 +0100

    added comment

--- router.go
+++ router.go
@@ -14,6 +14,7 @@ import (
 
 func handleRoutes(w webview.WebView) {
 
+    // route to quit application
     router.GET("/api/action/quit", func(c *gin.Context) {
         w.Terminate()
         os.Exit(0)
